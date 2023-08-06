473d3fdb000742941e0ef956772d6b2f943ea705
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2023-08-05 22:29:06 -0400

    Update Resume.js

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 
 const Resume = () => {
-  const resumeUrl = 'https://drive.google.com/file/d/1T7p8pFAxKSYjMvlc46_HFo_cTds3dYOo/view?usp=drive_link';
+  const resumeUrl = 'https://drive.google.com/file/d/1UVHzfYVY0tLtf185dofsEOwNryYHTVFa/view?usp=drive_link';
 
   return (
     <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
