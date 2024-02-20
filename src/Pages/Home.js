5c8f8ae8e2b40e9ea62aadd0f67c15087f6173f4
atishay2305-hub <atishay2305@gmail.com>
2024-02-19 23:12:51 -0500

    Fixed mobile view issues

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,30 +41,49 @@ function Home() {
       <div className="content">
         <div className="about">
           <div className="about-content">
-            <div className="summary-container">
-              <div className="summary-box">
-                <p className="summary-text" style={{ textAlign: "center" }}>
-                  Atishay Jain, Software Engineer
-                </p>
-                <h1 className="summary"><center>PROFESSIONAL SUMMARY</center></h1>
+          <div className="summary-container">
+  <div className="summary-box">
+    <p className="summary-text" style={{ textAlign: "center" }}>
+      Atishay Jain,
+      <br />
+      <span className="role">Software Engineer</span>
+    </p>
+  </div>
 
-                <p>
-                As a Software Engineer, I have earned a Master's in Software Engineering from Stevens Institute of Technology. My proficiency spans various programming languages, including Python, Java, and JavaScript, with expertise in full-stack development, testing methodologies, and agile practices. I have excelled in impactful roles in both the USA and India, demonstrating my ability to develop robust backend APIs, implement serverless architectures using AWS technologies, and conduct comprehensive testing with tools such as Selenium WebDriver and CircleCI. My contributions include enhancing user experiences in eCommerce web applications, conducting influential research on IoT in Sports, and leading teams in the development of innovative applications. My commitment to excellence is underscored by academic achievements, certifications, and successful project outcomes, making me a valuable asset to any software engineering team.
+  <h1 className="summary">
+    <center>PROFESSIONAL SUMMARY</center>
+  </h1>
 
-                </p>
+  <p>
+    As a Software Engineer, I have earned a Master's in Software Engineering
+    from Stevens Institute of Technology. My proficiency spans various
+    programming languages, including Python, Java, and JavaScript, with
+    expertise in full-stack development, testing methodologies, and agile
+    practices. I have excelled in impactful roles in both the USA and India,
+    demonstrating my ability to develop robust backend APIs, implement
+    serverless architectures using AWS technologies, and conduct
+    comprehensive testing with tools such as Selenium WebDriver and CircleCI.
+    My contributions include enhancing user experiences in eCommerce web
+    applications, conducting influential research on IoT in Sports, and
+    leading teams in the development of innovative applications. My
+    commitment to excellence is underscored by academic achievements,
+    certifications, and successful project outcomes, making me a valuable
+    asset to any software engineering team.
+  </p>
 
-                <div className="achievements-container">
-  <h1>ACHIEVEMENTS</h1>
-  <div className="achievements-box">
-    <ul>
-      <li>Provoste Master's Scholarship Awardee, Stevens Institute Of Technology, NJ</li>
-      <li>Graduate Student Assistant, Stevens Institute Of Technology, NJ</li>
-      <li>IEEE Student Member and Volunteer 2023, Stevens Institute Of Technology, NJ</li>
-    </ul>
+  <div className="achievements-container">
+    <h1>ACHIEVEMENTS</h1>
+    <div className="achievements-box">
+      <ul>
+        <li>Provoste Master's Scholarship Awardee, Stevens Institute Of Technology, NJ</li>
+        <li>Graduate Student Assistant, Stevens Institute Of Technology, NJ</li>
+        <li>IEEE Student Member and Volunteer 2023, Stevens Institute Of Technology, NJ</li>
+      </ul>
+    </div>
   </div>
 </div>
-              </div>
-            </div>
+
+
 
             <div className="skills-container">
               <div className="skills-box">
