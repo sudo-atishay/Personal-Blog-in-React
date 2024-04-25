b7c23e06b678f41de4165fbb6da3fae57ea93129
atishay2305-hub <atishay2305@gmail.com>
2024-04-25 16:56:39 -0400

    BUG FIX

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,7 +4,7 @@ import '../styles/Gallery.css';
 const Gallery = ({ images }) => {
   return (
     <div className="gallery-container">
-      <h2 className="gallery-title">Photos</h2>
+      <h2 className="gallery-title">PHOTOS</h2>
       <div className="gallery">
         {images.map((image, index) => (
           <img key={index} src={image} alt={`Image ${index + 1}`} className="gallery-img" />
