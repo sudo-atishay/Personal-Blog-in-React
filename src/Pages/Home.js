1a193cb05a6ed0067a6285b33d4369f5fbe21d79
Atishay Jain <atishay2305@gmail.com>
2025-07-31 20:53:18 -0400

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
             <span className="arrow">&gt;</span> Master's Graduate in Software Engineering from Stevens Institute of Technology, NJ 
           </div>
           <div className="status-item">
-            <span className="arrow">&gt;</span> Current Pursuits: Machine Learning, Maths, Algorithms & Problem Solving
+            <span className="arrow">&gt;</span> Current Pursuits: Machine Learning, Algorithms & Problem Solving
           </div>
         </div>
         
